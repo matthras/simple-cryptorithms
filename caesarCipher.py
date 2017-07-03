0c2d802a2a935e4c262438160ecd9a9b401354b0
matthras <matthew.y.p.mack@gmail.com>
2017-07-04 08:08:36 +1000

    Added two comments - more to come

--- caesarCipher.py
+++ caesarCipher.py
@@ -1,6 +1,3 @@
-
-
-
 # caesarCipher
 # message: the message you want to encrypt or decrypt
 # shift: the number of spaces you want to shift each letter
@@ -13,10 +10,14 @@ def caesarCipher(message, shift, strip=False):
     # If someone enters a number greater than 26, then we simply reduce by 26 until we get a number between 0 and 26.
     while shift > 26:
         shift -= 26
+    # Initialise the output as an empty string. The plan is to iterate through each letter of the message, translate it, and then put the result into translatedMessage.
     translatedMessage = ''
     
     for symbol in message:
         if symbol.isalpha():
+            # The easiest way to convert each alphanumeric character is to transform them into Unicode/ASCII, which assigns each alphanumeric character a number. We can simply then increase/decrease that number to 'shift' forwards/backwards in the cipher.
+            # See https://unicode-table.com/en/
+            
             unicodeNum = ord(symbol)
             unicodeNum += shift
             
